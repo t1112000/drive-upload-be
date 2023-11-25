1aea1878f7ee18f33c3d09ed0a4daaa1b83ef520
tuyen <tuyen.tran@eastplayers.io>
2023-11-25 13:02:32 +0700

    update: filter report

--- controllers/report.js
+++ controllers/report.js
@@ -1,7 +1,18 @@
 const ReportModel = require("../models/report");
 
 const getReports = async (req, res) => {
-  const reports = await ReportModel.find();
+  const { from, to } = req.query;
+
+  const query = {};
+
+  if (from && to) {
+    query.created_at = {
+      $gte: from,
+      $lt: to,
+    };
+  }
+
+  const reports = await ReportModel.find(query);
 
   return res.status(200).json({
     success: true,
